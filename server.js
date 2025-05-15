d2f1e895e01feb319f827d66ee8915199ddc4a2e
bobby12-1 <ifeanyichukwusamuel172@gmail.com>
2025-05-15 03:07:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const fs = require("fs");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
